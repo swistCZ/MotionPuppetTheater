7700c11c242acd9608decaa179a1a121fd4aeb2f
Zabi <Swist.m@gmail.com>
2026-08-13 22:36:51 +0200

    Updates

--- assets/BufferResource-Cx5ONqLo.js
+++ assets/BufferResource-Cx5ONqLo.js
@@ -1,4 +1,4 @@
-import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-DH2u2E64.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-BhBXA8fC.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
